3bb684570b5ab506cd099fc4f9cdcfdb2fac54a0
zxl20070701 <1904314465@qq.com>
2023-02-18 00:13:37 +0800

    校对版本号并添加必要的说明

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'index-page',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent {
-  title = '';
+
 }
